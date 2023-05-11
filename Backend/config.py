3f41ebab96a8a5d257f90662537a7c4c37232631
unknown <snsridhar@exafluence.com>
2023-05-11 15:47:25 -0700

    Backend docker added

--- Backend/config.py
+++ Backend/config.py
@@ -1,8 +1,15 @@
 #establish connection between Python and Mongo
 from pymongo import MongoClient
+import urllib.parse
 
-connection_string="mongodb://localhost:27017"
-client=MongoClient(connection_string)
+username = urllib.parse.quote_plus('Narayan')
+password = urllib.parse.quote_plus("Test@12345")
+
+url = "mongodb+srv://{}:{}@scmexpert.u5bh017.mongodb.net/?retryWrites=true&w=majority".format(username, password)
+
+# connection_string="mongodb://localhost:27017"
+client=MongoClient(url)
+
 
 #Creating DB
 db=client["SCMXpert"]
