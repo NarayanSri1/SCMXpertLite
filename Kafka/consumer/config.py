63604539a5500dd22a79a4c6ee1a305dd4c6cebd
unknown <snsridhar@exafluence.com>
2023-05-17 13:25:00 -0700

    local code commit

--- Kafka/consumer/config.py
+++ Kafka/consumer/config.py
@@ -1,18 +1,18 @@
-import urllib.parse
+# import urllib.parse
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from pymongo import MongoClient
 
-load_dotenv(dotenv_path=".env")
+# load_dotenv(dotenv_path=".env")
 
 
-username = urllib.parse.quote_plus(os.getenv("databaseusername"))
-password = urllib.parse.quote_plus(os.getenv("databasepassword"))
+# username = urllib.parse.quote_plus(os.getenv("databaseusername"))
+# password = urllib.parse.quote_plus(os.getenv("databasepassword"))
 
-databaseurl = "mongodb+srv://{}:{}@scmexpert.u5bh017.mongodb.net/?retryWrites=true&w=majority".format(username, password)
+# databaseurl = "mongodb+srv://{}:{}@scmexpert.u5bh017.mongodb.net/?retryWrites=true&w=majority".format(username, password)
 
-# client=MongoClient(os.getenv("databaselocal"))
-client=MongoClient(databaseurl)
+client=MongoClient(os.getenv("databaselocal"))
+# client=MongoClient(databaseurl)
 
 database=client["SCMXpert"]
 Device_Data_Stream=database["Device_Data_Stream"]
